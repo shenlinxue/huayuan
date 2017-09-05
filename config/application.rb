35ab0763af1c4d3b64e6b728d41b6038330fe45c
shenlinxue <842393426@qq.com>
2017-09-05 14:59:04 +0800

    init repostitory

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Huayuan
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |generator|
+		   generator.asset false
+		   generator.test_frameword false
+		   generator.skip_routes true
+		end
+  end
+end
