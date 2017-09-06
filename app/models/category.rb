179298fec7f6ed1b6bd5453c0962fed47e98a302
shenlinxue <842393426@qq.com>
2017-09-06 17:41:09 +0800

    admin

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,27 @@
 class Category < ApplicationRecord
-	has_ancestry
+
+	has_ancestry orphan_strategy: :destroy
+
+  has_many :products, dependent: :destroy
+
+	validates :title, presence: { message: "名称不能为空" }
+  validates :title, uniqueness: { message: "名称不能重复" }
+
+  
+  before_validation :correct_ancestry
+
+  def self.grouped_data
+    self.roots.order("weight desc").inject([]) do |result, parent|
+      row = []
+      row << parent
+      row << parent.children.order("weight desc")
+      result << row
+    end
+  end
+
+  private
+  def correct_ancestry
+    self.ancestry = nil if self.ancestry.blank?
+  end
+
 end
